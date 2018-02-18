914e2ae6e590ef5937eafa193c88b0851c8c023b
Veronica Bustabad <vbustabad@gmail.com>
2018-02-18 03:34:53 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,6 @@ def my_hash
   your_hash = {"key1" => "value1"}
 end
 
-
 def shipping_manifest
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
@@ -11,14 +10,7 @@ def retrieval
   shipping_manifest["oil paintings"]
 end
 
-# Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3,
-                  "muskets" => 2,
-                  "gun powder" => 4,
-                 }
+  shipping_manifest["muskets"] => 2
+  shipping_manifest["gun powder"] => 4
 end
